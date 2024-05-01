0e49ec3dc2ea911f51762c245248afdf5dbc5c2b
Happy <112599876+Itshappysad@users.noreply.github.com>
2024-05-01 17:55:49 -0500

    Delete src directory

--- src/components/Banner.tsx
+++
@@ -1,9 +0,0 @@
-const Banner = () => {
-  return (
-    <div className=" relative w-full  overflow-x-auto pb-14  h-72  ms-auto me-auto shadow-sm  place-items-stretch">
-      <img src="public\imgs\Test.webp" alt="this" />
-    </div>
-  );
-};
-
-export default Banner;
