1c5e8874dc406ada7aa850f2404d1fb54a08d6a5
NexmoCompanysisas <156712037+NexmoCompanysisas@users.noreply.github.com>
2024-05-12 17:48:51 -0500

    Add files via upload

--- src/core/storage.ts
+++ src/core/storage.ts
@@ -1,9 +1,9 @@
-import { getDownloadURL, ref } from 'firebase/storage';
-import { storage } from '../../firebase.config';
+import { getDownloadURL, ref } from "firebase/storage";
+import { storage } from "../../firebase.config";
 
 export async function getProductImage(id: string) {
   try {
-    const image = await getDownloadURL(ref(storage, `item-images/${id}.png`));
+    const image = await getDownloadURL(ref(storage, `item-images/${id}.webp`));
     return image;
   } catch (e) {
     return null;
