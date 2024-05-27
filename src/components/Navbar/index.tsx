9b8065536fe6353e4e83259fafee30a3bc68bb24
Nicolás López <cnexmo@gmail.com>
2024-05-27 23:30:48 +0200

    Bring back settings menu

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,19 @@
-import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink, useNavigate } from "react-router-dom";
 import { ShoppingCartContext } from "../../context/ShoppingCartContext";
 import "./styles.css";
 import { useContext } from "react";
 import cartImg from '../../assets/cart.jpeg';
+import { useAuth } from "../../context/useAuth";
+import { useModal } from "./hooks";
+import { Settings } from "../Settings";
 
 
 export function Navbar() {
+  const { user } = useAuth();
+  const { isOpen, handleOpen, handleClose } = useModal();
+  const navigate = useNavigate();
+
   const cartContext = useContext(ShoppingCartContext);
   if (!cartContext) {
     throw new Error('CartContext must be used within a CartProvider');
@@ -41,7 +48,39 @@ export function Navbar() {
             </div>
           </Nav.Link>
         </Nav>
+        <Button
+            onClick={
+              user
+                ? handleOpen
+                : () => {
+                    navigate("/signUp");
+                  }
+            }
+            style={{
+              width: "3rem",
+              height: "3rem",
+              position: "relative",
+              gap: ".5rem",
+            }}
+            variant="outline-secondary"
+          >
+            <svg
+              viewBox="2.5 2 31 24"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              width="40"
+              height="40"
+            >
+              <title />
+              <circle cx="12" cy="8" fill="currentColor" r="4" />
+              <path
+                d="M20,19v1a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V19a6,6,0,0,1,6-6h4A6,6,0,0,1,20,19Z"
+                fill="currentColor"
+              />
+            </svg>
+          </Button>
       </Container>
+      <Settings isOpen={isOpen} handleClose={handleClose} />
     </NavbarBs>
   );
 }
