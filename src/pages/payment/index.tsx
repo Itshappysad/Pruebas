793858c047bf0a086f71399397325ee3af209230
Nicolás López <cnexmo@gmail.com>
2024-05-28 16:39:56 +0200

    Remove items from cart after payment.

--- src/pages/payment/index.tsx
+++ src/pages/payment/index.tsx
@@ -4,6 +4,7 @@ import './styles.css';
 import { formatCurrency } from '../../utilities/formatCurrency';
 import { useAuth } from '../../context/useAuth';
 import { toast } from 'sonner';
+import { useNavigate } from 'react-router-dom';
 
 
 const PaymentPage = () => {
@@ -12,6 +13,7 @@ const PaymentPage = () => {
   if (!cartContext) {
     throw new Error('No se encontró el contexto para el carrito.');
   }
+  const navigate = useNavigate()
   const { cart } = cartContext;
   const totalPrice = cart.reduce((sum, item) => sum + item.price, 0);
   return (
@@ -65,6 +67,9 @@ const PaymentPage = () => {
         <button className='m-10 w-full btn btn-dark'
           onClick={() => {
             toast.success('Gracias por tu compra. La orden será creada y se enviará el producto cuando se confirme el pago.')
+            navigate("/")
+            localStorage.removeItem("shoppingCart")
+            window.location.reload();
           }}> Finalizar compra</button>
       </div>
       
